4bbeace6bfec24f7df760f037802de47019b5bdd
Preetam Rane <preetamrane2114@gmail.com>
2018-12-05 05:38:05 +0530

    Update Rotation.py

--- Opencv Py/Geometric Transformations/Rotation.py
+++ Opencv Py/Geometric Transformations/Rotation.py
@@ -1,9 +1,11 @@
 import cv2
 
-#read image
+img = cv2.imread('dragon.jpg')
+rows,cols = img.shape[:2]
 
-
-#Rotate it 
-
-
-#show result
+#rotate the image
+rotation_matrix = cv2.getRotationMatrix2D((cols/2, rows/2), 30, 1)
+img_rotation = cv2.warpAffine(img, rotation_matrix, (cols,rows))
+cv2.imshow('rotated_img',img_rotation)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
