1fa0e3158b8cefab39ba7685c9dc4cbc7c448560
Preetam Rane <preetamrane2114@gmail.com>
2018-12-05 05:37:29 +0530

    Update Perspective Transformation.py

--- Opencv Py/Geometric Transformations/Perspective Transformation.py
+++ Opencv Py/Geometric Transformations/Perspective Transformation.py
@@ -2,16 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-#read the image and get the shape
+img = cv2.imread('grid.jpg')
+rows,cols,ch = img.shape
 
+#draw circles at desired coordinates
+cv2.circle(img, (83, 90), 5, (0, 0, 255), -1)
+cv2.circle(img, (447, 90), 5, (0, 0, 255), -1)
+cv2.circle(img, (83, 472), 5, (0, 0, 255), -1)
+cv2.circle(img, (447, 472), 5, (0, 0, 255), -1)
 
-#mark the points
-
-
-#transformation points
-
-
-#Apply transforation
-
-
-#show the result
+#points where transformation is to be occured
+pts1 = np.float32([[83,90],[447,90],[83,472],[447,472]])
+pts2 = np.float32([[0, 0], [500, 0], [0, 600], [500, 600]])
+#perform transformation
+M = cv2.getPerspectiveTransform(pts1,pts2)
+#wrap the image
+dst = cv2.warpPerspective(img,M,(600,600))
+#plot the image results
+plt.subplot(121),plt.imshow(img),plt.title('Input')
+plt.subplot(122),plt.imshow(dst),plt.title('Output')
+plt.show()
